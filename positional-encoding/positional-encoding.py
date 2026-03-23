d3d7eddae2355048e9acbf427679b12551bca876
Saumit Paul <48136910+5aumit@users.noreply.github.com>
2026-03-22 20:40:37 -0400

    Update solution for positional-encoding

--- positional-encoding/positional-encoding.py
+++ positional-encoding/positional-encoding.py
@@ -5,14 +5,14 @@ def positional_encoding(seq_len, d_model, base=10000.0):
     Return PE of shape (seq_len, d_model) using sin/cos formulation.
     Odd d_model -> last column is sin.
     """
-    # Write code here
+
     pe = np.zeros((seq_len,d_model))
 
-    # Making columns : these will be the pos value in the formula since they contain the token index
+    # Making columns : these will be the 'pos' value in the formula since they contain the token index
     columns = np.arange(seq_len).reshape((seq_len,1))
-    # Making rows : these will be the i value in the formula since they contain the dim index
-    # We're adding 1+d/2 which will will always truncate for odd dim
-    # For even dim, we will need that extra dimension of sin
+    # Making rows : these will be the 'i' value in the formula since they contain the dim index
+    # We're adding 1+d/2 which will will always truncate for even dim
+    # For odd dim, we will need that extra dimension of sin
     rows = np.arange(int(d_model/2)+1).reshape((1,(1+int(d_model/2))))
 
     # Updating odd dims
